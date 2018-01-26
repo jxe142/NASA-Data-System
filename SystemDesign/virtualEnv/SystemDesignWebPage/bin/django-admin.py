aa7c8bd4a85287b5a417aad0851005db1c312528
Joel Evans <joelevans@Joels-MBP.wssu.edu>
2018-01-26 15:46:48 -0500

    First Commit of the project!

---
+++ SystemDesign/virtualEnv/SystemDesignWebPage/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joelevans/Desktop/Projects/Spring_2018/SystemDesign/virtualEnv/SystemDesignWebPage/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
