98e8bb896c3001f6abf6ff0c6f098e8230d49d1c
Joel Evans <joelevans@Joels-MBP.local>
2018-02-27 07:22:40 -0500

    Porting to heroku 7th try

--- DataWebPage/settings.py
+++ DataWebPage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -81,16 +82,18 @@ WSGI_APPLICATION = 'DataWebPage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nasaproject',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+DATABASES = { #Comment our becaue we use somthing else in production
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'nasaproject',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
+
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
