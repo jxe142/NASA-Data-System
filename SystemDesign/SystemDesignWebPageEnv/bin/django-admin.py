e3b2ff6cf794433728d5512e006253f2d162048a
Joel Evans <joelevans@Joels-MBP.local>
2018-02-27 03:38:04 -0500

    Changes to File Struct

--- SystemDesign/SystemDesignWebPageEnv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/joelevans/Desktop/Projects/NASA_Data_System/SystemDesign/SystemDesignWebPageEnv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
